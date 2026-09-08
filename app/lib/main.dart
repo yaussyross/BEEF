2616d6a88e71ccd9cbc6ec01fe536738186ff3c4
yaussyross <yaussyross@users.noreply.github.com>
2026-09-08 19:34:07 +0000

    Fix flutter analyze: remove unused import in main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'api/auth_api.dart';
 import 'api/http_client.dart';
